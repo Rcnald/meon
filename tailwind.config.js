8dcb3c37b63580cf10b15c7b42925a942e10b112
Rcnald <ronaldomjunior05@gmail.com>
2023-03-11 16:48:09 -0300

    header complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       },
       colors:{
         'color-main':'var(--main-color)',
+        'color-main-constrast':'var(--main-color-contrast)',
         'color-text':'var(--text-color)',
         'color-text-constrast':'var(--text-color-constrast)',
         'color-header':'var(--header-color)',
@@ -17,6 +18,9 @@ module.exports = {
         'color-placeholder':'var(--placeholder-color)',
         'color-whatsapp':'var(--whatsapp-color)',
         'color-facebook':'var(--facebook-color)',
+        'color-instagram':'var(--instagram-color)',
+        'color-linkedin':'var(--linkedin-color)',
+        'color-youtube':'var(--youtube-color)',
       },
     },
   },
