07aa12f47ed5bc97e51c7b03d78ba6ce8b282e1a
Rcnald <ronaldomjunior05@gmail.com>
2023-03-12 13:18:01 -0300

    menu process

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
+const header = document.getElementsByClassName('js-header')[0]
 let lastScrollTop = 0
-let header = document.getElementsByClassName('js-header')[0]
-window.addEventListener('scroll',function(){
+
+window.addEventListener('scroll', function(){
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
     if(scrollTop > lastScrollTop){
         header.classList.add('u-hide')
     }else{
@@ -9,3 +11,16 @@ window.addEventListener('scroll',function(){
     }
     lastScrollTop = scrollTop
 })
+
+const menuOpen = document.getElementsByClassName('js-menu-open')[0]
+const menuClose = document.getElementsByClassName('js-menu-close')[0]
+const navigationMenu = document.getElementsByClassName('js-navigation-list')[0]
+
+const toggleClick = function(button, elementToggled, utility){
+    button.addEventListener('click', function(){
+        elementToggled.classList.toggle(utility);
+    })
+}
+
+toggleClick(menuOpen,navigationMenu,'u-toggle')
+toggleClick(menuClose,navigationMenu,'u-toggle')
