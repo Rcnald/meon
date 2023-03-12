70115ebb8ad908be816b5743a00c1ffca6f0acd7
Rcnald <ronaldomjunior05@gmail.com>
2023-03-12 18:30:54 -0300

    process

--- src/js/main.js
+++ src/js/main.js
@@ -24,3 +24,16 @@ const toggleClick = function(button, elementToggled, utility){
 
 toggleClick(menuOpen,navigationMenu,'u-toggle')
 toggleClick(menuClose,navigationMenu,'u-toggle')
+
+const listItem = Array.from(document.getElementsByClassName('js-list-item'))
+const listContent = Array.from(document.getElementsByClassName('js-list-content'))
+
+const toggleClickSubmenu = function(button, elementToggled, utility){
+    button.forEach(function(el, i){
+        el.addEventListener('click', function(){
+            elementToggled[i].classList.toggle(utility)
+        })
+    })
+}
+
+toggleClickSubmenu(listItem, listContent, 'u-toggle')
